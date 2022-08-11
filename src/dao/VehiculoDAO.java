a1e1e0d524bb5ef54afcf99131d34b5754775980
V11Playko <heinnervega20@gmail.com>
2022-08-11 07:02:27 -0500

    Creando los metodos para la capa dao

--- src/dao/VehiculoDAO.java
+++ src/dao/VehiculoDAO.java
@@ -1,2 +1,22 @@
-package dao;public class VehiculoDAO {
+package dao;
+
+import model.VehiculoModel;
+
+public class VehiculoDAO {
+
+    public static void crearVehiculoDB(VehiculoModel Vehiculo){
+
+    }
+
+    public static void leerAtributosVehiculoDB(){
+
+    }
+
+    public static void eliminarVehiculoDB(int id_vehiculo){
+
+    }
+
+    public static void actualizarVehiculoDB(VehiculoModel Vehiculo) {
+        
+    }
 }
